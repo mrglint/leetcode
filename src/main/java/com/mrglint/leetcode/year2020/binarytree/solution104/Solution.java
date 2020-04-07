6ee100c2c82fc23f90085bdc9548cfd67b14ede7
mrglint <mrglint@163.com>
2020-04-08 07:48:10 +0800

    maximum-depth-of-binary-tree

---
+++ src/main/java/com/mrglint/leetcode/year2020/binarytree/solution104/Solution.java
@@ -0,0 +1,16 @@
+package com.mrglint.leetcode.year2020.binarytree.solution104;
+
+import com.mrglint.leetcode.TreeNode;
+
+/**
+ * @author luhuancheng
+ * @since 2020-04-08 07:46
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
